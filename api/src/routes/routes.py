2bc033827af752db1021a76b28184a5720e36136
Sparsh Srivastava <ss6381@columbia.edu>
2023-03-02 22:16:49 -0800

    update terraform directory name

--- api/src/routes/routes.py
+++ api/src/routes/routes.py
@@ -5,3 +5,8 @@ app_instance = Blueprint(name="app_instance", import_name=__name__)
 @app_instance.route("/")
 def index():
     return "Hello World!"
+
+
+@app_instance.route("/login")
+def login():
+    return "Login!"
